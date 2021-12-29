8507b8d10f15b8d7ba10c2593b3183dc1e8ea35f
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-12-29 15:08:51 +0200

    add components

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './styles/App.scss'
+
+class App extends Component {
+  render() { 
+   return <>dssdfsdsdfsdfsdf</>
+  }
+}
+ 
+export default App;
